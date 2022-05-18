1da98e712b27dacc13293288c76484077076ba5c
ThikerXCX <rifkimuhammad967@gmail.com>
2022-05-18 14:46:00 +0700

    tes

---
+++ app/src/main/java/com/muhamadrifki192102139/recyclerview/MahasiswaModel.java
@@ -0,0 +1,38 @@
+package com.muhamadrifki192102139.recyclerview;
+
+public class MahasiswaModel {
+    private String nim,nama,jenisKelamin,jP;
+    public MahasiswaModel(){
+
+    }
+    public String getNim(){
+        return nim;
+    }
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getjP() {
+        return jP;
+    }
+
+    public void setjP(String jP) {
+        this.jP = jP;
+    }
+}
